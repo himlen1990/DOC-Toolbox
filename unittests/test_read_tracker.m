2f7c5a3aa69faec1d3a9140c8120629077e21906
Robert Walecki <rw2614@imperial.ac.uk>
2015-06-04 16:34:21 +0100

    bug: unittests fail if not matlab 2015

---
+++ unittests/test_read_tracker.m
@@ -0,0 +1,13 @@
+%
+% works only under matlab 2015 
+%
+%addpath('../DOC')
+%load('./test_landmarks.mat')
+%set.iter = 50;
+%set.parallel = 0;
+%set.raw_landmarks = 1;
+%mod = seq.tr('MLR',data,[0,0],set);
+%pre = seq.pr(data,mod);
+%test_seq = packages.READ_TRACKER('./test_read_tracker/');
+%test_seq{1}.Y = 1;
+%pre = seq.pr(test_seq,mod);
